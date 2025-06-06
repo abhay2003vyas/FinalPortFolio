bb5f8f2c30a88800afd40728fa33ef11c458a5a6
Abhay Vyas <vyas0994@gmail.com>
2025-06-06 18:06:32 +0530

    update splash effect

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import SplashCursor from './SplashCursor'
 function App() {
   const [load, upadateLoad] = useState(true);
 
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <Router>
+      <SplashCursor />
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
