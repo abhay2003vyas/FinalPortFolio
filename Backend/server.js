bbc5d783f08b6c49287fae2dd74530bfd000b6dd
Abhay Vyas <vyas0994@gmail.com>
2025-06-06 18:06:05 +0530

    update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const router = require('./router');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./router");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000; // Use the port from the environment variable
@@ -11,16 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api', router);
+app.use("/api", router);
 
 // Database connection
 mongoose
-  .connect('mongodb+srv://abhay:sjdd3rtet8dsuh@cluster0.l8ty0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+  .connect(
+    "mongodb+srv://abhay:abhay12345@cluster0.l8ty0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    // "mongodb+srv://abhay:sjdd3rtet8dsuh@cluster0.l8ty0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Start the server
 app.listen(port, () => {
